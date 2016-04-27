5b54d4cd9a7a1923e89f70b16133e4afab1d3f20
cece0719 <cece0719@naver.com>
2016-04-28 02:39:10 +0900

    dsafsad

---
+++ src/main/java/com/woowol/suinCar/controller/TestController.java
@@ -0,0 +1,16 @@
+package com.woowol.suinCar.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@EnableAutoConfiguration
+public class TestController {
+	@RequestMapping(value = "/")
+	public String test(Model model) {
+		model.addAttribute("msg", "helloWorld");
+		return "test";
+	}
+}
