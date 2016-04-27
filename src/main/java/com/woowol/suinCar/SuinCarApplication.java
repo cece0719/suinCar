5c3ddedf606ef93eb66c09a37403bf3f7d4b3be2
cece0719 <cece0719@naver.com>
2016-04-28 01:44:40 +0900

    Initial commit

---
+++ src/main/java/com/woowol/suinCar/SuinCarApplication.java
@@ -0,0 +1,12 @@
+package com.woowol.suinCar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuinCarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuinCarApplication.class, args);
+	}
+}
