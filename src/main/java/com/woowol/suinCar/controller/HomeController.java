2622807eacc0408746d5288ff38121b4382f385a
cece0719 <cece0719@naver.com>
2016-04-30 16:58:13 +0900

    dsafsad

--- src/main/java/com/woowol/suinCar/controller/HomeController.java
+++ src/main/java/com/woowol/suinCar/controller/HomeController.java
@@ -2,34 +2,37 @@ package com.woowol.suinCar.controller;
 
 
 import com.pi4j.io.gpio.*;
+import com.pi4j.util.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
 
 @Controller
 public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
 	@RequestMapping(value = "/")
-	public synchronized String home(Model model) throws InterruptedException {
+	public synchronized String home(@RequestParam Map<String, String> params, Model model) throws InterruptedException {
 		final GpioController gpio = GpioFactory.getInstance();
 		final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_04, "MyLED");
 		pin.setShutdownOptions(true);
-		Thread.sleep(1000);
-		pin.high();
-		Thread.sleep(1000);
-		pin.toggle();
-		Thread.sleep(1000);
-		pin.toggle();
-		Thread.sleep(1000);
-		pin.toggle();
+		if (StringUtil.isNotNullOrEmpty(params.get("led"))) {
+			if ("on".equals(params.get("led"))) {
+				pin.high();
+			} else {
+				pin.low();
+			}
+		}
 		gpio.shutdown();
 		gpio.unprovisionPin(pin);
 
 		logger.info("hello~!");
-		model.addAttribute("msg", "helloWorld");
+		model.addAttribute("msg", pin.isHigh()?"on":"off");
 		return "home";
 	}
 }
--- src/main/resources/templates/home.ftl
+++ src/main/resources/templates/home.ftl
@@ -5,6 +5,8 @@
     <title>${msg}</title>
 </head>
 <body>
+<a href="/?led=on">led on</a>
+<a href="/?led=off">led off</a>
 ${msg}
 </body>
 </html>
