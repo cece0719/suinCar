0d65a2edc08183557e8f37b86506a774554e9ab2
cece0719 <cece0719@naver.com>
2016-04-30 15:39:06 +0900

    dsafsad

--- src/main/java/com/woowol/suinCar/controller/HomeController.java
+++ src/main/java/com/woowol/suinCar/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.woowol.suinCar.controller;
 
 
+import com.pi4j.io.gpio.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -12,9 +13,23 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
 	@RequestMapping(value = "/")
-	public String home(Model model) {
+	public String home(Model model) throws InterruptedException {
+		final GpioController gpio = GpioFactory.getInstance();
+		final GpioPinDigitalOutput pin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_23, "MyLED", PinState.HIGH);
+		pin.setShutdownOptions(true, PinState.LOW);
+		Thread.sleep(500);
+		pin.low();
+		Thread.sleep(500);
+		pin.toggle();
+		Thread.sleep(500);
+		pin.toggle();
+		Thread.sleep(500);
+		pin.pulse(1000, true);
+		gpio.shutdown();
+
 		logger.info("hello~!");
 		model.addAttribute("msg", "helloWorld");
 		return "home";
 	}
 }
+
